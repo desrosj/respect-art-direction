39e67d64ff4f7f828e5ea47b958c419b5c96b14a
Jonathan Desrosiers <desrosj@gmail.com>
2017-05-10 21:55:07 -0400

    Update unit tests.

--- tests/test-source-lists.php
+++ tests/test-source-lists.php
@@ -18,16 +18,86 @@ class Test_Image_Sources extends WP_UnitTestCase {
 
 		$this->assertEquals( $rad_source_lists, array() );
 
-		rad_add_source_list( 'test_source', array( 'image_size_1', 'image_size_2' ) );
-
-		$this->assertEquals( $rad_source_lists, array(
-			'test_source' => array(
+		rad_add_source_list( 'test_source', array(
+			'breakpoint1' => array(
 				'image_size_1',
 				'image_size_2',
 			),
+			'breakpoint2' => array(
+				'image_size_3',
+				'image_size_4'
+			),
 		) );
+
+		$this->assertEquals( $rad_source_lists, array(
+			'test_source' => array(
+				'breakpoint1' => array(
+					'image_size_1',
+					'image_size_2',
+				),
+				'breakpoint2' => array(
+					'image_size_3',
+					'image_size_4'
+				),
+			),
+		) );
+
+		$rad_source_lists = array();
 	}
 
+	function test_rad_add_to_source_list() {
+		global $rad_source_lists;
+
+		rad_add_to_source_list( 'my_source_list', 'my_image_size' );
+
+		$this->assertEquals( $rad_source_lists, array(
+			'my_source_list' => array(
+				'default' => array(
+					'my_image_size',
+				),
+			),
+		) );
+
+		$rad_source_lists = array();
+
+		rad_add_to_source_list( 'my_source_list', 'my_image_size', 'my_breakpoint' );
+
+		$this->assertEquals( $rad_source_lists, array(
+			'my_source_list' => array(
+				'my_breakpoint' => array(
+					'my_image_size',
+				),
+			),
+		) );
+
+		rad_add_to_source_list( 'my_source_list', 'my_image_size2', 'my_breakpoint' );
+
+		$this->assertEquals( $rad_source_lists, array(
+			'my_source_list' => array(
+				'my_breakpoint' => array(
+					'my_image_size',
+					'my_image_size2',
+				),
+			),
+		) );
+
+		rad_add_to_source_list( 'my_source_list', 'my_image_size3', 'my_breakpoint2' );
+
+		$this->assertEquals( $rad_source_lists, array(
+			'my_source_list' => array(
+				'my_breakpoint' => array(
+					'my_image_size',
+					'my_image_size2',
+				),
+				'my_breakpoint2' => array(
+					'my_image_size3',
+				),
+			),
+		) );
+
+		$rad_source_lists = array();
+	}
+	
 	/**
 	 * Test rad_image_size_is_image_source().
 	 */
@@ -35,5 +105,7 @@ class Test_Image_Sources extends WP_UnitTestCase {
 		rad_add_source_list( 'another_test_source' );
 
 		$this->assertTrue( rad_image_size_is_image_source( 'another_test_source' ) );
+
+		$rad_source_lists = array();
 	}
 }
