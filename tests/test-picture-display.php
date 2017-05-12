ba3090eca8be02fea34458ecfbc642b42094e66d
Jon Desrosiers <desrosj@gmail.com>
2017-05-11 22:08:10 -0400

    Refactor the test a bit. Add test for when no valid source list name is provided.

--- tests/test-picture-display.php
+++ tests/test-picture-display.php
@@ -11,34 +11,49 @@
 class Test_Picture_Display extends WP_UnitTestCase {
 
 	/**
-	 * Test that the Post Thumbnail HTML is properly overridden.
+	 * Post ID of test page.
+	 *
+	 * @var int
 	 */
-	function test_rad_the_post_thumbnail_with_art_direction() {
-		global $rad_source_lists;
+	public $post_id;
 
-		$post_id = $this->factory->post->create( array(
-			'post_title' => 'A Test Page',
-			'post_type' => 'page',
-		) );
+	/**
+	 * Post ID of test attachment.
+	 *
+	 * @var int
+	 */
+	public $attachment_id;
 
+	/**
+	 * Test image file path.
+	 *
+	 * @var string
+	 */
+	public $orig_file = DIR_TESTDATA . '/images/33772.jpg';
+
+	/**
+	 * Test file upload path.
+	 *
+	 * @var string
+	 */
+	public $test_file;
+
+	/**
+	 * Set up test.
+	 *
+	 * - Add image sizes.
+	 * - Add a few breakpoints.
+	 * - Add a source list.
+	 * - Create test page.
+	 * - Create test attachment post.
+	 * - Assign attachment as featured image on the test page.
+	 */
+	function setUp() {
 		add_image_size( 'test_image_size_1', 1200, 600, true );
 		add_image_size( 'test_image_size_2', 600, 300, true );
 		add_image_size( 'test_image_size_3', 300, 150, true );
 		add_image_size( 'test_image_size_4', 1000, 800, true );
 		add_image_size( 'test_image_size_5', 500, 400, true );
-
-		$orig_file = DIR_TESTDATA . '/images/33772.jpg';
-		$test_file = '/tmp/33772.JPG';
-		copy( $orig_file, $test_file );
-
-		$attachment_id = $this->factory->attachment->create_object( $test_file, 0, array(
-			'post_mime_type' => 'image/jpg',
-		) );
-
-		$this->assertNotEmpty( $attachment_id );
-
-		// Add as featured image.
-		update_post_meta( $post_id, '_thumbnail_id', $attachment_id );
 
 		// Register breakpoints and source lists for this test.
 		rad_add_breakpoint( 'large', '(min-width: 1001px)' );
@@ -56,16 +71,63 @@ class Test_Picture_Display extends WP_UnitTestCase {
 			),
 		) );
 
-		$markup = get_the_post_thumbnail( $post_id, 'my_source_list' );
+		$this->post_id = $this->factory->post->create( array(
+			'post_title' => 'A Test Page',
+			'post_type' => 'page',
+		) );
 
-		$this->assertEquals( '<picture>
-<source media="(min-width: 1001px)" srcset="http://example.org/wp-content/uploads//tmp/33772.JPG 1w, http://example.org/wp-content/uploads//tmp/33772.JPG 1w, http://example.org/wp-content/uploads//tmp/33772.JPG 1w" />
-<source media="(max-width: 1000px)" srcset="http://example.org/wp-content/uploads//tmp/33772.JPG 1w, http://example.org/wp-content/uploads//tmp/33772.JPG 1w" />
-<img src="http://example.org/wp-content/uploads//tmp/33772.JPG" alt="Detail of the above quilt, highlighting the embroidery and exotic stitchwork." />
-</picture>', $markup );
+		$this->test_file = '/tmp/33772.jpg';
+		copy( $this->orig_file, $this->test_file );
 
-		// Cleanup.
+		$this->attachment_id = $this->factory->attachment->create_object( $this->test_file, 0, array(
+			'post_mime_type' => 'image/jpg',
+		) );
+
+		$this->assertNotEmpty( $this->attachment_id );
+
+		// Add as featured image.
+		update_post_meta( $this->post_id, '_thumbnail_id', $this->attachment_id );
+	}
+
+	/**
+	 * Test the regular image markup is output if no source list name is passed.
+	 */
+	function test_rad_the_post_thmbnail_with_invalid_source_list_name() {
+		$expected = '<img src="http://example.org/wp-content/uploads//tmp/33772.jpg" class="attachment-my_fake_source_list size-my_fake_source_list wp-post-image" alt="" />';
+		$actual = $actual = get_the_post_thumbnail( $this->post_id, 'my_fake_source_list' );
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Test that the Post Thumbnail HTML is properly overridden.
+	 */
+	function test_rad_the_post_thumbnail_with_art_direction() {
+		$expected = '<picture>
+<source media="(min-width: 1001px)" srcset="http://example.org/wp-content/uploads//tmp/33772.jpg 1w, http://example.org/wp-content/uploads//tmp/33772.jpg 1w, http://example.org/wp-content/uploads//tmp/33772.jpg 1w" />
+<source media="(max-width: 1000px)" srcset="http://example.org/wp-content/uploads//tmp/33772.jpg 1w, http://example.org/wp-content/uploads//tmp/33772.jpg 1w" />
+<img src="http://example.org/wp-content/uploads//tmp/33772.jpg" alt="Detail of the above quilt, highlighting the embroidery and exotic stitchwork." />
+</picture>';
+		$actual = get_the_post_thumbnail( $this->post_id, 'my_source_list' );
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Remove all changes from this test class.
+	 */
+	function tearDown() {
+		global $rad_source_lists, $rad_breakpoints;
+
 		$rad_source_lists = array();
-		unlink( $test_file );
+		$rad_breakpoints = array();
+
+		remove_image_size( 'test_image_size_1', 1200, 600, true );
+		remove_image_size( 'test_image_size_2', 600, 300, true );
+		remove_image_size( 'test_image_size_3', 300, 150, true );
+		remove_image_size( 'test_image_size_4', 1000, 800, true );
+		remove_image_size( 'test_image_size_5', 500, 400, true );
+
+		unlink( $this->test_file );
 	}
 }
