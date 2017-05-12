1419197d2e7f101f51f60db1c76ab01f6cc5efe5
Jon Desrosiers <desrosj@gmail.com>
2017-05-11 21:18:12 -0400

    Re-adding Code Climate reporting file with correct permissions.

---
+++ bin/codeclimate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+php vendor/bin/test-reporter --stdout > codeclimate.json
+curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.4.4)' https://codeclimate.com/test_reports
